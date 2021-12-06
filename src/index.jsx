35cd50bdbc23239fbffb859c5a687181a697c8e5
leninner <mazabandalenin180@gmail.com>
2021-12-05 22:38:19 -0500

    [UPDATE]

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './routes/App';
+import { UnderConstruction } from './utils/UnderConstruction';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<UnderConstruction />, document.getElementById('root'));
---
+++ src/utils/UnderConstruction.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from './styles';
+import { GlobalStyles } from '../GlobalStyles';
+
+export const UnderConstruction = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Container>
+        <h3>Comming Soon</h3>
+      </Container>
+    </>
+  );
+};
