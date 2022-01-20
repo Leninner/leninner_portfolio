f5ce8029be392ae298a8e53c010e43159d227ee1
leninner <mazabandalenin180@gmail.com>
2022-01-20 11:33:08 -0500

    Rebase

--- src/components/HireMe/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { HireMe, HireMeText } from './style';
-
-export const HireMeComponent = () => (
-  <HireMe as='a' href='https://wa.link/0sy727' target='_blank'>
-    <HireMeText>HIRE ME</HireMeText>
-  </HireMe>
-);
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { GlobalStyles } from '../styles/GlobalStyles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Layout } from '../containers/Layout';
-import { Projects } from '../pages/projects';
-import { Home } from '../pages/Home';
 import { Preview } from '../pages/Preview';
 
 export const App = () => {
   return (
     <BrowserRouter>
       <GlobalStyles />
-      <Layout>
-        <Routes>
-          <Route path='/' element={<Preview />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/home' element={<Home />} />
-        </Routes>
-      </Layout>
+
+      <Routes>
+        <Route path='/' element={<Preview />} />
+      </Routes>
     </BrowserRouter>
   );
 };
