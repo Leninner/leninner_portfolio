6e3ec626dfa4d538332d9d89fb9280790fc22900
leninner <mazabandalenin180@gmail.com>
2022-03-08 16:46:46 -0500

    Added images for the projects

--- src/assets/data/projectsData.js
+++ src/assets/data/projectsData.js
@@ -1,4 +1,8 @@
 import { TechsInfo } from './techsInfo';
+import shoppingifyList from '../../utils/projectsPhotos/shoppingifyList.png';
+import battleship from '../../utils/projectsPhotos/battleship.png';
+import ropaShop from '../../utils/projectsPhotos/ropaShop.png';
+import mini from '../../utils/projectsPhotos/mini.png';
 
 const { React, Redux, StyledComponents, ReactRouter, TailwindCSS, Sass, JavaScript, HTML } = TechsInfo;
 
@@ -8,18 +12,18 @@ export const projectsData = [
     title: 'Shoppingify List',
     description:
       'Shoppingify List allows you to take your shopping list wherever you go. You can add items to your list and also, you can see the history of your shopping list. All your data is stored in your browser, so you can always come back to it.',
-    image: '',
+    image: shoppingifyList,
     features: ['Latest web technology', 'Easy to scaling', 'Very user friendly'],
     techInfo: [React, Redux, StyledComponents, ReactRouter],
     preview: 'https://shoppingify-project.vercel.app/',
-    repo: '',
+    repo: 'https://github.com/Leninner/shoppingify-list',
   },
   {
     id: 2,
     title: 'Battleship Game',
     description:
       'BattleShip is a game where you try to destroy your opponent’s fleet. You can play with the computer and see who is the best player.',
-    image: '',
+    image: battleship,
     features: ['Modern and responsive', 'Great user experience', 'Very easy to use'],
     techInfo: [JavaScript, Sass, HTML],
     preview: 'https://leninner.github.io/battleship/',
@@ -29,20 +33,20 @@ export const projectsData = [
     id: 3,
     title: 'Ropa Shop',
     description: 'The best way to grow is to help and teach other passionate people to grow.',
-    image: '',
+    image: ropaShop,
     features: ['Teach logical thinking', 'Use a workflow similar to reality', 'Use the best practices in demand'],
     techInfo: [React, Redux, ReactRouter, StyledComponents],
-    preview: 'https://shoppingify-project.vercel.app/',
-    repo: '',
+    preview: 'https://ropa-shop.vercel.app/',
+    repo: 'https://github.com/Leninner/e-anime-shop',
   },
   {
     id: 4,
     title: 'Mini|Big Challenges',
     description: 'The best way to grow is to help and teach other passionate people to grow.',
-    image: '',
+    image: mini,
     features: ['Teach logical thinking', 'Use a workflow similar to reality', 'Use the best practices in demand'],
     techInfo: [React, ReactRouter, TailwindCSS],
-    preview: 'https://shoppingify-project.vercel.app/',
-    repo: '',
+    preview: 'https://mini-challenges.vercel.app/',
+    repo: 'https://github.com/Leninner/mini_challenges_react_platzi',
   },
 ];
